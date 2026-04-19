984569875e6e9e495ce9999c2ee4605e6b848fa7
LoreDreamer <educordero@icloud.com>
2026-04-19 00:06:00 -0400

    Initial commit for the branch

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,23 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import hi from '../assets/hi.png';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+        <IonToolbar className="custom-toolbar">
+          <IonTitle> 
+            <div className="item">
+              <img src={hi} alt="Logo" className="item-image" />
+              <span className="item-text">Inicio</span>
+              <span className="item-text">Alertas</span>
+              <span className="item-text">Consejos</span>
+              <span className="item-text">Normativa</span>
+              <span className="rounded-box">Acceder</span>
+            </div> 
+          </IonTitle> 
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
