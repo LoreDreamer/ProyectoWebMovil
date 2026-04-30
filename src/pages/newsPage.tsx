921c6050863f71c6966c52e7a0fad07269546ac3
LoreDreamer <educordero@icloud.com>
2026-04-30 13:10:14 -0400

    Typing error

--- src/pages/newsPage.tsx
+++ src/pages/newsPage.tsx
@@ -1,7 +1,7 @@
 import { IonPage, IonContent } from '@ionic/react';
 import { Navbar } from '../components/Navbar';
 import { NewsPart } from '../components/newsPart';
-import './alertsNews.css';
+import './newsPage.css';
 
 export const NewsPage: React.FC = () => {
   return (
